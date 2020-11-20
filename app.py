60543d24792624fbb2c28f9a876e8fd834db1fc2
aymen-ziouche <aymenannaba16@gmail.com>
2020-11-20 16:46:46 +0100

    modifying app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -9,6 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
