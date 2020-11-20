9d240a9dcbdf49e95254ba8dd04233f6926fddc8
aymen-ziouche <aymenannaba16@gmail.com>
2020-11-20 19:40:39 +0100

    storing the user data in a file

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 
+path = "simple.txt"
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+
+        req = request.form
+
+        name = req.get("name")
+        email = req["email"]
+        message = request.form["message"]
+        print(req)
+        with open(path, "a+") as c:
+            c.write(" email : " + str(email) + " name : " +
+                    str(name) + " message : " + str(message) + '\n')
+            c.seek(0)
+
+        return redirect(request.url)
+    return render_template('index.html',)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#path_1 = "simple.txt"
+#
+#
+# def read_file_content(path):
+#
+#    with open(path_1, "r+") as c:
+#        c.write(first + '\n')
+#        c.write(second + '\n')
+#        c.seek(0)
+#        c.read()
+#
+# read_file_content(path_1)
