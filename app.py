7dfc29ac3ca4dafe52ae6317e33dd56dda34dd31
aymen-ziouche <aymenannaba16@gmail.com>
2020-11-22 19:01:02 +0100

    clean up

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -17,9 +16,9 @@ def index():
         message = request.form["message"]
         print(req)
         with open(path, "a+") as c:
-            c.write(" email : " + str(email) + " name : " +
-                    str(name) + " message : " + str(message) + '\n')
-            c.seek(0)
+            c.write(" email : " + str(email) + "," + " name : " +
+                    str(name) + "," + " message : " + str(message) + '\n')
+            # c.seek(0)
 
         return redirect(request.url)
     return render_template('index.html',)
@@ -27,17 +26,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-#path_1 = "simple.txt"
-#
-#
-# def read_file_content(path):
-#
-#    with open(path_1, "r+") as c:
-#        c.write(first + '\n')
-#        c.write(second + '\n')
-#        c.seek(0)
-#        c.read()
-#
-# read_file_content(path_1)
